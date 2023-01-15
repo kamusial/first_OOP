f36c7aa30e9c9a2a43cbf273374883f2fb83a71b
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-01-15 11:27:47 +0100

    2 programy_v2

--- second_OOP.py
+++ second_OOP.py
@@ -1,18 +1,43 @@
 class Audi:
-    def __init__(self):
-        self.kolor = 'czerwony'
+    def __init__(self, barwa, info_wprowadzone):
+        self.kolor = barwa
         self.ilosc_paliwa = 10
-        self.kondycja = 4
+        self.kondycja = info_wprowadzone
+        self.tryb_ekonomiczny = False
+        self.spalanie_na_100 = 14
     def __str__(self):
         napis = (f'Audi ma kolor {self.kolor} i jest w kondycji {self.kondycja}')
         return napis
+    def zasieg(self):
+        zasieg = self.ilosc_paliwa / self.spalanie_na_100 * 100
+        return round(zasieg)
+    def ustaw_tryb(self, tryb):
+        self.tryb = tryb
+        if self.tryb == 'eco':
+            self.spalanie_na_100 = 10
+            self.tryb_ekonomiczny = True
+            print('Tryb eco')
+        elif self.tryb == 'normal':
+            self.spalanie_na_100 = 14
+            self.tryb_ekonomiczny = False
+            print('Tryb normal')
+        else:
+            print('tryb nierozpoznany, brak zmian')
 
-moje_auto = Audi()
-print(moje_auto.ilosc_paliwa)
-print(moje_auto.kondycja)
-moje_auto.kondycja += 1
-print(moje_auto.kondycja)
+    def wlacz_eco(self):
+        self.spalanie_na_100 = 10
+        self.tryb_ekonomiczny = True
+        print('Tryb eco włączony')
 
-auto_sasiada = Audi()
+    def wlacz_normal(self):
+        self.spalanie_na_100 = 14
+        self.tryb_ekonomiczny = False
+        print('Tryb normal włączony')
+
+moje_auto = Audi('czerwony', 4)
+auto_sasiada = Audi('zielony', 5)
 print(auto_sasiada.kondycja)
 print(moje_auto)
+print(moje_auto.zasieg())
+moje_auto.ustaw_tryb('eco')
+print(moje_auto.zasieg())
